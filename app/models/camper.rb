81a1f69d00ce1a69beca466a0b8558d226c16cf0
ashabahmed <ashabahmed7@gmail.com>
2020-09-22 11:25:44 -0400

    done.

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,2 +1,6 @@
 class Camper < ApplicationRecord
+    has_many :signups
+    has_many :activities, through: :signups
+    validates :name, uniqueness: true
+    validates :age, numericality: {less_than: 18, greater_than: 8}
 end
